779aac64d413b20760b30fd4d8012fb9964d4f11
Flauwed <jackson@bress.net>
2026-08-02 16:48:25 -0500

    market data script introduced

---
+++ src/market_test.py
@@ -0,0 +1,33 @@
+import requests
+import json
+
+def read_player_id():
+    with open("player_id", "r") as f:
+        player_id = f.read()
+    return player_id
+
+def read_auth_token():
+    with open("auth.tok", "r") as f:
+        auth_tok = f.read()
+    return auth_tok
+
+def save_market_data(data):
+    with open("market_data.json","w") as f:
+        f.write(json.dumps(data))
+
+
+url = "https://esi.evetech.net/markets/prices"
+
+headers = {
+    "Accept-Language": "en",
+    "If-None-Match": "",
+    "X-Compatibility-Date": "2026-07-21",
+    "X-Tenant": "",
+    "If-Modified-Since": "",
+    "Accept": "application/json"
+}
+
+response = requests.get(url, headers=headers)
+
+save_market_data(response.json)
+
