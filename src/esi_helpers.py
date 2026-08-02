762b6975ad3ae79b66b2ca06987b4583c8e8af0a
Jetorific <jtrom@illinois.edu>
2026-08-02 17:27:14 -0500

    refactored esi_helpers

--- src/esi_helpers.py
+++ src/esi_helpers.py
@@ -1,53 +1,80 @@
 import requests
 
-def get_wallet_balance(vars):
-    url = f"https://esi.evetech.net/characters/{vars['player_id']}/wallet"
+class ESIHelper:
+    vars = None
 
-    headers = {
-        "Accept-Language": "",
-        "If-None-Match": "",
-        "X-Compatibility-Date": vars["compatibility_date"],
-        "X-Tenant": "",
-        "If-Modified-Since": "",
-        "Accept": "application/json",
-        "Authorization": vars["auth_token"]
-    }
+    def __init__(self, vars):
+        self.vars = vars
 
-    response = requests.get(url, headers=headers)
+    def names_to_ids(self, names):
+        url = "https://esi.evetech.net/universe/ids"
 
-    return response.json()
+        headers = {
+            "Accept-Language": "",
+            "If-None-Match": "",
+            "X-Compatibility-Date": self.vars["compatibility_date"],
+            "X-Tenant": "",
+            "If-Modified-Since": "",
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
 
-def get_character_location(vars):
-    url = "https://esi.evetech.net/characters/2124597207/location"
+        if isinstance(names, str):
+            names = [names]
+        else:
+            names = list(names)
+        response = requests.post(url, json=names, headers=headers)
 
-    headers = {
-        "Accept-Language": "",
-        "If-None-Match": "",
-        "X-Compatibility-Date": vars["compatibility_date"],
-        "X-Tenant": "",
-        "If-Modified-Since": "",
-        "Accept": "application/json",
-        "Authorization": vars["auth_token"]
-    }
+        return response.json()
 
-    response = requests.get(url, headers=headers)
+    def get_wallet_balance(self):
+        url = f"https://esi.evetech.net/characters/{self.vars['player_id']}/wallet"
 
-    return response.json()
+        headers = {
+            "Accept-Language": "",
+            "If-None-Match": "",
+            "X-Compatibility-Date": self.vars["compatibility_date"],
+            "X-Tenant": "",
+            "If-Modified-Since": "",
+            "Accept": "application/json",
+            "Authorization": self.vars["auth_token"]
+        }
 
-def set_autopilot_waypoint(destination, vars, add_to_beginning="false", clear_other_waypoints="false"):
-    url = "https://esi.evetech.net/ui/autopilot/waypoint"
+        response = requests.get(url, headers=headers)
 
-    querystring = {"add_to_beginning":add_to_beginning,"clear_other_waypoints":clear_other_waypoints, "destination_id":destination}
+        return response.json()
 
-    headers = {
-        "Accept-Language": "",
-        "If-None-Match": "",
-        "X-Compatibility-Date": vars["compatibility_date"],
-        "X-Tenant": "",
-        "If-Modified-Since": "",
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        "Authorization": vars["auth_token"]
-    }
+    def get_character_location(self):
+        url = "https://esi.evetech.net/characters/2124597207/location"
 
-    response = requests.post(url, headers=headers, params=querystring)
+        headers = {
+            "Accept-Language": "",
+            "If-None-Match": "",
+            "X-Compatibility-Date": self.vars["compatibility_date"],
+            "X-Tenant": "",
+            "If-Modified-Since": "",
+            "Accept": "application/json",
+            "Authorization": self.vars["auth_token"]
+        }
+
+        response = requests.get(url, headers=headers)
+
+        return response.json()
+
+    def set_autopilot_waypoint(self, destination, add_to_beginning="false", clear_other_waypoints="false"):
+        url = "https://esi.evetech.net/ui/autopilot/waypoint"
+
+        querystring = {"add_to_beginning":add_to_beginning,"clear_other_waypoints":clear_other_waypoints, "destination_id":destination}
+
+        headers = {
+            "Accept-Language": "",
+            "If-None-Match": "",
+            "X-Compatibility-Date": self.vars["compatibility_date"],
+            "X-Tenant": "",
+            "If-Modified-Since": "",
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": self.vars["auth_token"]
+        }
+
+        response = requests.post(url, headers=headers, params=querystring)
--- src/test.py
+++ src/test.py
@@ -11,12 +11,15 @@ def read_auth_token():
     return auth_tok
 
 def main():
-    vars = {
+    esi = ESIHelper({
         "auth_token" : read_auth_token(),
         "player_id" : read_player_id(),
         "compatibility_date" : "2026-07-21"
-    }
+    })
 
+    print(esi.names_to_ids("Sirppala"))
+
+    return
     print(get_wallet_balance(vars))
     set_autopilot_waypoint("30002801", vars, clear_other_waypoints="true")
 
